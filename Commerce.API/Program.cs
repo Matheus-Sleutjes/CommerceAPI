d493e6aa17ea17af34be774bec4b0cc2d58f2d56
Matheus Sleutjes <matheus.sleutjes@loglabdigital.com.br>
2024-03-15 11:41:45 -0400

    inclusão EntityFramework, configuração e ajuste de launch

--- Commerce.API/Program.cs
+++ Commerce.API/Program.cs
@@ -1,3 +1,6 @@
+using Commerce.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Context 
+builder.Services.AddDbContext<CommerceContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
+var serviceScope = app.Services.GetService<IServiceScopeFactory>().CreateScope();
+var context = serviceScope.ServiceProvider.GetRequiredService<CommerceContext>();
+context.Database.Migrate();
+
 
 // Configure the HTTP request pipeline.
 
---
+++ Commerce.Infrastructure/Context/CommerceContext.cs
@@ -0,0 +1,10 @@
+﻿using Commerce.Domain.Entitie;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commerce.Infrastructure.Context
+{
+    public class CommerceContext(DbContextOptions<CommerceContext> options) : DbContext(options)
+    {
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
